6e85e2e6e1a422d091c7370918b3b72cb41df554
sshussai <sshussai@ualberta.ca>
2017-10-30 22:36:30 -0600

    Make basic UI for creating a HabitType

--- HabiTrack/app/src/main/java/com/example/habitrack/MainActivity.java
+++ HabiTrack/app/src/main/java/com/example/habitrack/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.habitrack;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQ_HTYPE_CREATE = 1;
+    private Button createButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        createButton = (Button) findViewById(R.id.createHabitButtonID);
+
+        createButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v){
+                Intent newHabitTypeIntent = new Intent(v.getContext(), NewHabitTypeActivity.class);
+                //startActivityForResult(newHabitTypeIntent, REQ_HTYPE_CREATE);
+                startActivity(newHabitTypeIntent);
+            }
+        }
+        );
+
     }
 }
--- HabiTrack/app/src/main/java/com/example/habitrack/NewHabitTypeActivity.java
+++ HabiTrack/app/src/main/java/com/example/habitrack/NewHabitTypeActivity.java
@@ -2,12 +2,43 @@ package com.example.habitrack;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class NewHabitTypeActivity extends AppCompatActivity {
+
+    private String title;
+    private String reason;
+
+    private Button submitButton;
+    private EditText titleBox;
+    private EditText reasonBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_habit_type);
+
+        submitButton = (Button) findViewById(R.id.submitButton);
+        titleBox = (EditText) findViewById(R.id.titleBox);
+        reasonBox = (EditText) findViewById(R.id.reasonBox);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createHabitType();
+            }
+        }
+        );
+
+    }
+
+    private void createHabitType(){
+
+        title = titleBox.getText().toString();
+        reason = reasonBox.getText().toString();
+
+        finish();
     }
 }
