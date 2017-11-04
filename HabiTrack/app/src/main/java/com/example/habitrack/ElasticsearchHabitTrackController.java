3ba1b72bf40acbf6b8772f410b00eebca24f725d
pennyfea <pennyfea@ualberta.ca>
2017-11-03 20:41:40 -0600

    Elastic search attempt 1

---
+++ HabiTrack/app/src/main/java/com/example/habitrack/ElasticsearchHabitTrackController.java
@@ -0,0 +1,38 @@
+package com.example.habitrack;
+
+import android.os.AsyncTask;
+
+import com.searchly.jestdroid.JestDroidClient;
+
+/**
+ * Created by pennyfea on 11/3/17.
+ */
+
+public class ElasticsearchHabitTrackController {
+
+    private static JestDroidClient client;
+
+    // TODO we need a function which adds habitType, habitEvents, and profiles to elastic search
+    public static class AddHabitType {
+        @Override
+        protected Object doInBackground(Object[] objects) {
+            return null;
+        }
+    }
+
+    public static class AddProfile {
+        @Override
+        protected Object doInBackground(Object[] objects) {
+            return null;
+        }
+    }
+
+    public static class AddHabitEvent {
+        @Override
+        protected Object doInBackground(Object[] objects) {
+            return null;
+        }
+    }
+
+
+}
