0ca62cd2ffa3f7dc05dfbe58eed71d53bed0e56d
sshussai <sshussai@ualberta.ca>
2017-12-04 23:33:39 -0700

    fix the delete htmd function call

--- HabiTrack/app/src/main/java/com/example/habitrack/HabitTypeDetailsActivity.java
+++ HabiTrack/app/src/main/java/com/example/habitrack/HabitTypeDetailsActivity.java
@@ -189,7 +189,7 @@ public class HabitTypeDetailsActivity extends AppCompatActivity {
             deleteButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    htc.deleteHabitType(htID);
+                    htc.deleteHabitType(esID);
                     finish();
                 }
             });
